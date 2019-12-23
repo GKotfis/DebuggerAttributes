25872ac4770b0a8d3f4449b92ef1f3ed31e46874
Grzegorz Kotfis <gkotfis@outlook.com>
2019-12-23 16:40:06 +0100

    override ToString() in Person class

--- External.Sdk/Person.cs
+++ External.Sdk/Person.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 
-namespace External.Sdk
-{
-    public class Person
-    {
+namespace External.Sdk {
+    public class Person {
         public int Id { get; set; }
         public string Name { get; set; }
         public Address Address { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}";
+        }
     }
 
-    public class Address
-    {
+    public class Address {
         public string City { get; set; }
         public string Street { get; set; }
     }
