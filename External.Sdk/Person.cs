779f69c15228d2ab0f9efeb4d7c3e1bea174a479
Grzegorz Kotfis <gkotfis@outlook.com>
2019-12-23 16:45:59 +0100

    simple usage of DebuggerDisplay

--- External.Sdk/Person.cs
+++ External.Sdk/Person.cs
@@ -1,18 +1,17 @@
-﻿using System;
+﻿using System.Diagnostics;
 
-namespace External.Sdk {
-    public class Person {
+namespace External.Sdk
+{
+    [DebuggerDisplay("Id: {Id, nq}, Name: {Name, nq}")]
+    public class Person
+    {
         public int Id { get; set; }
         public string Name { get; set; }
         public Address Address { get; set; }
-
-        public override string ToString()
-        {
-            return $"Id: {Id}, Name: {Name}";
-        }
     }
 
-    public class Address {
+    public class Address
+    {
         public string City { get; set; }
         public string Street { get; set; }
     }
